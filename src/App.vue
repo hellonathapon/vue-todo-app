d923642b306a7d7b8bfecb8b405c17f70f2b67a7
hellonathapon <hellonathapon@gmail.com>
2021-01-09 12:40:14 +0700

    Structure main components with grid

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <Sidebar/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+import Sidebar from './components/Sidebar.vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
+    Sidebar,
     HelloWorld
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    display: grid;
+    grid-template-columns: 250px auto;
+    grid-template-areas: "a b";
+  }
 </style>
---
+++ src/components/FormAddTodo.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="add-todo">
+        <b-form-input placeholder="Enter your Todo" class="form"></b-form-input>
+        <b-button pill variant="outline-info">Button</b-button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FormAddTodo',
+}
+</script>
+
+<style scoped>
+    .add-todo {
+        grid-area: a;
+        grid-row: 1 / 3;
+        background: linear-gradient(87deg,#2dce89,#2dcecc);
+        padding-top: 2rem;
+    }
+    .form {
+        width: 80%;
+        margin: 0 auto;
+    }
+</style>
